9b249bc13fdc1ab6dcd79c4c98a7c089b72e67fd
Guy <guybs@mta.ac.il>
2018-08-24 19:58:22 +0300

    new

--- C18 Ex03 Guy 301002762 Omer 204059331/Ex03.GarageLogic/Program.cs
+++ C18 Ex03 Guy 301002762 Omer 204059331/Ex03.GarageLogic/Program.cs
@@ -8,32 +8,74 @@ namespace Ex03.GarageLogic
     {
         List<Vehicle> vehicles;
     }
-    public abstract class Vehicle
-    {
-        private string m_ModelName;//Should be const?
-        private string m_LicsenseID;//Should be const?
-        private float m_EnergyLeft;
-        public Engine m_Engine;//not sure if it should be private or public, i think public.
-    }
     public abstract class Engine
     {
-        //All the virtual methods
+        private float m_AmountOfEnergyLeft;
+        private float m_MaximumAmountOfEnergy;
+        public void Recharge(float i_fill)
+        {
+            if ((i_fill + m_AmountOfEnergyLeft) <= m_MaximumAmountOfEnergy)
+            {
+                m_AmountOfEnergyLeft += i_fill;
+            }
+
+
+
+        }
+
+        public float MaximumAmountOfEnergy
+        {
+            get { return m_MaximumAmountOfEnergy; }
+            set { m_MaximumAmountOfEnergy = value; }
+        }
+
+        public float ReaminingAmount
+        {
+            get { return m_AmountOfEnergyLeft; }
+            set { m_AmountOfEnergyLeft = value; }
+        }
     }
     public class GasEngine : Engine
     {
-        string m_GasType;//const
-        private float m_CurrentGasLiters;
-        private float m_MaxGasLiters;//Const
+        private string m_GasType;//const
 
-        public bool FillGas(float i_NumOfLitersToFill,string i_GasType)
+        public string GasType
         {
-
+            get { return m_GasType; }
+            set { m_GasType = value; }
         }
     }
     public class ElectricEngine : Engine
     {
 
     }
+
+    public abstract class Vehicle
+    {
+        private string m_ModelName;//Should be const?
+        private string m_LicsenseID;//Should be const?
+        private float m_PrecentageOfEnergyLeft;
+        public Engine m_Engine;//not sure if it should be private or public, i think public.
+        public Wheel m_Whells; //array of whells
+        public string ModelName
+        {
+            get { return m_ModelName; }
+            set { m_ModelName = value; }
+        }
+
+        public string LicsenseID
+        {
+            get { return m_LicsenseID; }
+            set { m_LicsenseID = value; }
+        }
+
+        public float PrecentageOfEnergyLeft
+        {
+            get { return m_PrecentageOfEnergyLeft; }
+            set { m_PrecentageOfEnergyLeft = value; }
+        }
+    }
+    
     public class  Bike : Vehicle
     {
         private string m_LicenseType;//Const?
@@ -49,6 +91,7 @@ namespace Ex03.GarageLogic
         private bool isTrunkCool;//const    
         private float m_TrunkVolume;//const
     }
+
     public class Wheel
     {
         private string m_NameOfManufacturer;//Const?
@@ -57,7 +100,12 @@ namespace Ex03.GarageLogic
 
         public bool BlowUpWheel(float i_AirToBlow)
         {
-
+            bool tirePressureUpdated = false;
+            if ((i_AirToBlow+m_CurrentAirPressure)<=m_MaxAirPressure)
+            {
+                tirePressureUpdated = true;
+            }
+            return tirePressureUpdated;
         }
     }
     
